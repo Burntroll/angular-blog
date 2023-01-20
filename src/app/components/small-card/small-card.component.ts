7c65e1890660f43816c0e417e2434b984247c031
Francisco Jose da Silva Sousa Junior <fco.silva.dev@gmail.com>
2023-01-20 22:04:56 +0300

    Add new photos and make data binding

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string = "";
+  @Input()
+  cardTime: string = "";
+  @Input()
+  cardTitle: string = "";
   constructor() { }
 
   ngOnInit(): void {
